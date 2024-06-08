a4358a2d5a1727c2fd1c41c66807b4f5208d1186
rishav.saxena <rishav.saxena@razorpay.com>
2024-06-08 17:12:13 +0530

    Ramp up project using spring initializer

---
+++ src/main/java/com/MyCricket/MyCricket/MyCricketApplication.java
@@ -0,0 +1,13 @@
+package com.MyCricket.MyCricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCricketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyCricket/MyCricket/MyCricketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyCricket.MyCricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
